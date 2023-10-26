08aed1090e836cdbe2a9bed3a1a7c5ec85b99689
Mmy2000 <yahianada005@gmail.com>
2023-10-26 04:13:10 +0300

    display product in store pagepart two

--- store/views.py
+++ store/views.py
@@ -4,5 +4,10 @@ from .models import Product
 
 def store(request):
     product = Product.objects.all().filter(is_available=True)
+    product_count = product.count()
 
-    return render(request , 'store/store.html',{'product':product})
+    context = {
+        'product' : product ,
+        'product_count' : product_count
+    }
+    return render(request , 'store/store.html',context)

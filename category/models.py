8b1b8adb8faea892ab0aefc48ca26343aa75e3dd
Mmy2000 <yahianada005@gmail.com>
2023-10-26 03:30:29 +0300

    creating store model and add product

--- category/models.py
+++ category/models.py
@@ -1,14 +1,11 @@
 from django.db import models
-from django.utils.text import slugify 
 
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(unique=True, max_length=50)
     slug = models.SlugField(unique=True, max_length=100)
     description = models.TextField( max_length=2000 , blank=True)
     image = models.ImageField( upload_to='cat_img' , blank=True)
-
 
     def __str__(self):
         return self.name

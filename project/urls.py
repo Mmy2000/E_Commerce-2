0ba57fbc1237dbe2522747a651f9eb09116c1dc9
Mmy2000 <yahianada005@gmail.com>
2023-10-26 04:03:49 +0300

    display product in store page

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('settings.urls' , namespace='home')),
+    path('store/' , include('store.urls' , namespace='store')),
 ]
 
 urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def store(request):
+    product = Product.objects.all().filter(is_available=True)
+
+    return render(request , 'store/store.html',{'product':product})

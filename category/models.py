f24e3b3c8b317d57050016f5fa545498c3c9f249
Mmy2000 <yahianada005@gmail.com>
2023-10-26 05:30:01 +0300

    display product by category part two

--- category/models.py
+++ category/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -6,6 +7,9 @@ class Category(models.Model):
     slug = models.SlugField(unique=True, max_length=100)
     description = models.TextField( max_length=2000 , blank=True)
     image = models.ImageField( upload_to='cat_img' , blank=True)
+
+    def get_url(self):
+        return reverse('store:product_by_category',args=[self.slug])
 
     def __str__(self):
         return str(self.name)

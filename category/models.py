ff9b16e4d1fdaf196995d345aea3fa241215463c
Mmy2000 <yahianada005@gmail.com>
2023-10-26 03:05:41 +0300

    creating auto slug

--- category/models.py
+++ category/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify 
 
 
 # Create your models here.
@@ -8,6 +8,7 @@ class Category(models.Model):
     slug = models.SlugField(unique=True, max_length=100)
     description = models.TextField( max_length=2000 , blank=True)
     image = models.ImageField( upload_to='cat_img' , blank=True)
+
 
     def __str__(self):
         return self.name

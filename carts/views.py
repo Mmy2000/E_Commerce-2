e3028fba4d35040b20b7557bb73c5394721389dd
Mmy2000 <yahianada005@gmail.com>
2023-10-27 05:12:42 +0200

    creating view function for carts prt three

--- carts/views.py
+++ carts/views.py
@@ -44,11 +44,15 @@ def carts(request ,total=0 ,quantity=0,cart_item=None ):
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += cart_item.quantity
+        tax = (2 * total)/100
+        grand_total = total+tax
     except Cart.DoesNotExist:
         pass
 
     context = {'total':total,
                'quantity':quantity , 
-               'cart_items':cart_items
+               'cart_items':cart_items,
+               'tax' : tax , 
+               'grand_total':grand_total,
                }
     return render(request , 'carts/cart.html',context)

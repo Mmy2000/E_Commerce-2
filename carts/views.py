25672ff72c29e2c276c2a19e5e8baecbb0e5824c
Mmy2000 <yahianada005@gmail.com>
2023-10-27 04:49:13 +0200

    creating view function for carts prt two

--- carts/views.py
+++ carts/views.py
@@ -37,5 +37,18 @@ def add_cart(request , product_id):
 
 
 
-def carts(request):
-    return render(request , 'carts/cart.html')
+def carts(request ,total=0 ,quantity=0,cart_item=None ):
+    try :
+        cart = Cart.objects.get(cart_id = _cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart , is_active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+    except Cart.DoesNotExist:
+        pass
+
+    context = {'total':total,
+               'quantity':quantity , 
+               'cart_items':cart_items
+               }
+    return render(request , 'carts/cart.html',context)

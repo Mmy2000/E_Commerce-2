04ba419cc3f84be4117c8b01bd07e538c1c39a97
Mmy2000 <yahianada005@gmail.com>
2023-10-27 04:19:52 +0200

    creating view function for carts prt one

--- carts/models.py
+++ carts/models.py
@@ -16,5 +16,5 @@ class CartItem(models.Model):
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.product
+        return str(self.product)
     
--- carts/urls.py
+++ carts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import carts 
+from . views import carts , add_cart
 
 app_name = 'carts'
 
 urlpatterns = [
     path('', carts ,name = 'cart'),
+    path('add_cart/<int:product_id>/', add_cart ,name = 'add_cart'),
 ]

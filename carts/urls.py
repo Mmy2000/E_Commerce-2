7e1fccba3201771cadb16558a4a6ec7375de221c
Mmy2000 <yahianada005@gmail.com>
2023-10-27 23:11:55 +0200

    creating view function for carts prt four

--- carts/urls.py
+++ carts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . views import carts , add_cart
+from . views import carts , add_cart , remove_cart
 
 app_name = 'carts'
 
 urlpatterns = [
     path('', carts ,name = 'cart'),
     path('add_cart/<int:product_id>/', add_cart ,name = 'add_cart'),
+        path('remove_cart/<int:product_id>/', remove_cart ,name = 'remove_cart'),
 ]

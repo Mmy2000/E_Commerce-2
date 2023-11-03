d92911cbc6a21ca7473f4383ef81697b564495fe
Mmy2000 <yahianada005@gmail.com>
2023-11-03 13:14:25 +0200

    updating

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Payment , Order , OrderProduct
 
 # Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user','email','order_total','created_at','is_orderd')
+    list_editable = ('is_orderd',)
+
 admin.site.register(Payment)
 admin.site.register(OrderProduct)
-admin.site.register(Order)
+admin.site.register(Order,OrderAdmin)

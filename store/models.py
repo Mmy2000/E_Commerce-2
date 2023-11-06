37817c33b3e57eef402359d2d1c17c8e73636e04
Mmy2000 <yahianada005@gmail.com>
2023-11-06 20:20:05 +0200

    create review model

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
@@ -43,4 +44,18 @@ class Variation(models.Model):
 
     def __str__(self):
         return self.variation_value
+    
+class ReviewRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=500 , blank=True)
+    review = models.TextField(max_length=500 , blank=True)
+    rating = models.FloatField()
+    ip = models.CharField( max_length=50 , blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField( auto_now_add=True)
+    updated_at = models.DateTimeField( auto_now=True)
+
+    def __str__(self):
+        return self.subject
     

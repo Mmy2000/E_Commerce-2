55b1574d3dfdd52297603c3da9225daa75e22126
Mmy2000 <yahianada005@gmail.com>
2023-11-07 13:31:33 +0200

    check if the product is ordered or not

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render , get_object_or_404 , redirect
 from .models import Product , ReviewRating
 from category.models import Category
 from carts.models import CartItem
+from orders.models import OrderProduct
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 from django.db.models.query_utils import Q
@@ -44,9 +45,15 @@ def product_detail(request,category_slug,product_slug):
         in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request),product=single_product).exists()
     except Exception as e:
         raise e
+    
+    try:
+        orderproduct = OrderProduct.objects.filter(user=request.user , product_id=single_product.id).exists()
+    except OrderProduct.DoesNotExist:
+        orderproduct = None
     context = {
         'single_product':single_product,
         'in_cart':in_cart,
+        'orderproduct':orderproduct,
     }
     return render(request,'store/product_detail.html',context)
 

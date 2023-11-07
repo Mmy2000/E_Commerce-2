5dec126a6b6d81e303afb5c2dfb7b3e7b46e025c
Mmy2000 <yahianada005@gmail.com>
2023-11-07 14:30:22 +0200

    display rating stars in the product detail page

--- store/models.py
+++ store/models.py
@@ -1,4 +1,4 @@
-from django.db.models import Avg
+from django.db.models import Avg , Count
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -25,6 +25,13 @@ class Product(models.Model):
         if reviews['average'] is not None:
             avg = float(reviews['average'])
         return avg
+    
+    def count_review(self):
+        reviews = ReviewRating.objects.filter(product=self , status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
 
     def __str__(self):
         return self.name

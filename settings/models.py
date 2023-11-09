a53b82a035f6ad95ceafa3487106e2b7ff077ba5
Mmy2000 <yahianada005@gmail.com>
2023-11-09 02:07:02 +0200

    customize profile and fixing image uploud

--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class HomeImage(models.Model):
+    name = models.CharField( max_length=50 ,default="image")
+    home_image=models.ImageField(upload_to='home images/')
+
+    def __str__(self):
+        return self.name
--- settings/views.py
+++ settings/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from store.models import Product
+from .models import HomeImage
 # Create your views here.
 def home(request):
     product = Product.objects.all().filter(is_available=True)
+    image = HomeImage.objects.all()
 
     return render(request, 'settings/home.html',{
-        'product' : product
+        'product' : product,
+        'image':image,
     })

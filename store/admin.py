b53c6f0d36ef104a4798e2ea1a55a155828e8667
Mmy2000 <yahianada005@gmail.com>
2023-11-14 01:11:04 +0200

    creating product gallary model

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Product , Variation , ReviewRating
+from .models import Product , Variation , ReviewRating , ProductGallary
+import admin_thumbnails
 # Register your models here.
+@admin_thumbnails.thumbnail('image')
+class ProductGallaryInline(admin.TabularInline):
+    model = ProductGallary
+    extra = 1
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name' , 'price' , 'stock' , 'category' , 'modified_date' , 'is_available')
     prepopulated_fields = {'slug' : ('name',)}
-
+    inlines = [ProductGallaryInline]
 
 
 class VariationAdmin(admin.ModelAdmin):
@@ -15,4 +21,6 @@ class VariationAdmin(admin.ModelAdmin):
 admin.site.register(Variation,VariationAdmin)
 admin.site.register(Product,ProductAdmin)
 admin.site.register(ReviewRating)
+admin.site.register(ProductGallary)
+
 
--- store/models.py
+++ store/models.py
@@ -73,3 +73,10 @@ class ReviewRating(models.Model):
     def __str__(self):
         return self.subject
     
+class ProductGallary(models.Model):
+    product = models.ForeignKey(Product,  on_delete=models.CASCADE , default=None)
+    image = models.ImageField( upload_to="product_gallary", max_length=255)
+
+    def __str__(self):
+        return str(self.product.name)
+    

df4a26ac5e139330aa30223e7b7e2d5d3c06c637
Mmy2000 <yahianada005@gmail.com>
2023-11-16 01:24:02 +0200

    display categories in category list page

--- orders/urls.py
+++ orders/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('place_order/' , views.place_order , name='place_order'),
     path('payments/' , views.payments , name='payments'),
     path('orders/' , views.payments , name='orders'),
+    path('order_complete/', views.order_complete, name='order_complete'),
 ]
